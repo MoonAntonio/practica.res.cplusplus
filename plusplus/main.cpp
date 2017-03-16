9d7f95d43ced53989ece45c4e3e710ea2432d3a5
MoonAntonio <pinchodev91@gmail.com>
2017-03-16 16:24:50 +0100

    Notacion cientifica

--- plusplus/main.cpp
+++ plusplus/main.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 int main()
 {
+    // NOTACION CIENTIFICA
+    int valor1 = 3e3;//3*10^3
+    double valor2 = 5.3e3;//5.3*10^3
+    double valor3 = 1.222e-3;//1.222*10^3
+    cout<<valor1<<endl;
+    cout<<valor2<<endl;
+    cout<<valor3<<endl;
+
+    cout<<"\n"<<endl;
+    // ---------------------------------------------
+
     // ASIGNACION DE VARIABLES
     int edad = 30,edadTotal;
     char nombre = 'Moon';
@@ -16,8 +27,10 @@ int main()
     bool esperando = false;
 
     edadTotal = 25;
-    cout << nombre << endl;
+    cout << "Nombre: " << nombre << endl;
 
+
+    cout<<"\n"<<endl;
     // ---------------------------------------------
 
     /* TIPOS DE VARIABLES
