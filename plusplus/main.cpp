2d0f4afe0898a9a4dba2ccb85ff928b9239febe7
MoonAntonio <pinchodev91@gmail.com>
2017-03-16 15:31:13 +0100

    Secuencias de escape

--- plusplus/main.cpp
+++ plusplus/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    /*SECUENCIAS DE ESCAPE
+    \n -> salto de linea
+    \b -> retroceso
+    \n -> retorno hasta el inicio
+    \t -> tabulador*/
+    cout << "Hello" << "world!" << endl;
+    cout << "Hello\n" << "world!" << endl;
+    cout << "Hello\b" << "world!" << endl;
+    cout << "Hello\n" << "world!" << endl;
+    cout << "Hello\t" << "world!" << endl;
     return 0;
 }
