b0d0ee5965845e38fad2829a07df464e258922c1
MoonAntonio <pinchodev91@gmail.com>
2017-03-16 17:05:58 +0100

    Add project operaciones

---
+++ operacionesplusplus/main.cpp
@@ -0,0 +1,11 @@
+// Practica para refrescar la sintaxis de C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // ---------------------------------------------
+
+    return 0;
+}
