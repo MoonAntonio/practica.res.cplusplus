f45ca8de2928ed2da770c86b095d166bda91df1c
MoonAntonio <pinchodev91@gmail.com>
2017-03-16 17:19:14 +0100

    Operadores matematicos

--- operacionesplusplus/main.cpp
+++ operacionesplusplus/main.cpp
@@ -7,5 +7,21 @@ int main()
 {
     // ---------------------------------------------
 
+    /* OPERADORES MATEMATICOS
+        Suma -> +
+        Resta -> -
+        Multiplicacion -> *
+        Division -> /
+        Modulo -> %
+    */
+
+    int numero1 = 7;
+    int numero2 = 3;
+    int cociente = numero1/numero2;
+    int mod = numero1%numero2;
+
+    cout<<cociente<<endl;
+    cout<<mod<<endl;
+
     return 0;
 }
