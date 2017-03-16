819ce486b2811ec86b3afb3e46bffa30a54ad26e
MoonAntonio <pinchodev91@gmail.com>
2017-03-16 15:32:53 +0100

    Fix escape

--- plusplus/main.cpp
+++ plusplus/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
     /*SECUENCIAS DE ESCAPE
     \n -> salto de linea
-    \b -> retroceso
-    \n -> retorno hasta el inicio
+    \b -> retroceso (No elimina, solo retrocede)
+    \r -> intercambia la primera y la segunda
     \t -> tabulador*/
     cout << "Hello" << "world!" << endl;
     cout << "Hello\n" << "world!" << endl;
     cout << "Hello\b" << "world!" << endl;
-    cout << "Hello\n" << "world!" << endl;
+    cout << "Hello\r" << "world!" << endl;
     cout << "Hello\t" << "world!" << endl;
     return 0;
 }
