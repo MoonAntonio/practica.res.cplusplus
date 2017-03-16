46da6059ac60b218de77e5a8b3207b0f6b1d9f9b
MoonAntonio <pinchodev91@gmail.com>
2017-03-16 15:44:09 +0100

    Mensajes por consola

--- plusplus/main.cpp
+++ plusplus/main.cpp
@@ -1,14 +1,27 @@
+
+// Practica para refrescar la sintaxis de C++
+
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+
+
+    /* MENSAJE POR CONSOLA
+    cout << "Hello world!" << endl;
+    */
+    cout << "Hello world!" << endl;
+
+
     /*SECUENCIAS DE ESCAPE
     \n -> salto de linea
     \b -> retroceso (No elimina, solo retrocede)
     \r -> intercambia la primera y la segunda
-    \t -> tabulador*/
+    \t -> tabulador
+    */
     cout << "Hello" << "world!" << endl;
     cout << "Hello\n" << "world!" << endl;
     cout << "Hello\b" << "world!" << endl;
