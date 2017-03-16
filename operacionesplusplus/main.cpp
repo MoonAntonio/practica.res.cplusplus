903eda370fc28f13c47cf19e9bccee3c6873e080
MoonAntonio <pinchodev91@gmail.com>
2017-03-16 17:27:43 +0100

    Precedencia aritmetica

--- operacionesplusplus/main.cpp
+++ operacionesplusplus/main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main()
 {
+    /* PRECEDENCIA ARITMETICA
+    1. - Las operaciones entre parentesis
+    2. - Multiplicacion, division y modulo
+    3. - Suma y resta
+    */
+
+    cout<<14-8/2*3+1<<endl;// 3
+    cout<<(14-8)/2*3+1<<endl;// 10
+    cout<<14 - (8/2*3)+1<<endl;// 3
+    cout<<14-8/2*(3+1)<<endl;// -2
+    cout<<(14-8)/2*(3+1)<<endl;// 12
+
+    cout<<"\n"<<endl;
+    // ---------------------------------------------
+
     // DIVISION ENTERA Y DIVISION FLOAR/REAL
 
     cout<<7<<endl;
