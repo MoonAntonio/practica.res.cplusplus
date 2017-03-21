e28c7de35d71c54523ad8840c27a2ebe5ba4695b
MoonAntonio <pinchodev91@gmail.com>
2017-03-21 19:36:40 +0100

    Ejercicio 1

---
+++ Condicionales/Ejercicio 1 Numero Par/ejercicio1.cpp
@@ -0,0 +1,24 @@
+// Practica para refrescar la sintaxis de C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Escriba un programa que diga si un numero entero ingresado es par o impar
+
+    int numero;
+    cout<<"Ingrese el numero:";
+    cin>>numero;
+
+    if(numero%2==0)
+    {
+        cout<<"El numero es par"<<endl;
+    }
+    else
+    {
+        cout<<"El numero es impar"<<endl;
+    }
+
+    return 0;
+}
