0203685182ef6b4eab83ecf0202c50a907f05789
Moon Antonio <pinchodev91@gmail.com>
2018-01-15 07:30:19 +0100

    Ejer 2

--- Ejercicios/eje2.cpp
+++ Ejercicios/eje2.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    float numero;
+    float suma = 0;
+
+    do
+    {
+        cout<<"Numero:";
+        cin>>numero;
+        if(numero >= 0)
+        {
+            suma = suma + numero;
+        }
+    }while(numero >= 0);
+
+    cout<<"Total "<<suma<<endl;
+
+    return 0;
+}
