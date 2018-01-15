1a16fdc81ff57bfadba63c6e90fcddfc7faebf5a
Moon Antonio <pinchodev91@gmail.com>
2018-01-15 07:08:20 +0100

    For en c++

--- Estructura Repetitivas/For/main.cpp
+++ Estructura Repetitivas/For/main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main()
 {
-    for(int n = 0;n < 10; n++)
-
-    cout << "Hello world!" << endl;
+    for(int n = 0;n <= 10; n++)
+    {
+        cout << n << endl;
+    }
     return 0;
 }
